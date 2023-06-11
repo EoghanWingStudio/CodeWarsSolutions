558a558c8fb47c9f6c1a54a958de980571fa41e6
EoghanWingStudio <eoghan@wing.studio>
2023-06-11 12:52:13 +0400

    odd Int test

---
+++ CodeWarsSolutions/OddInt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsSolutions
+{
+    public static class OddInt
+    {
+        public static int find_it(int[] ints)
+        {
+            return ints.GroupBy(x => x).Where(x => x.Count() % 2 != 0).Select(m => m.Key).FirstOrDefault();
+
+        }
+    }
+}
---
+++ CodeWarsSolutionsTest/OddIntTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsSolutionsTest
+{
+    public class OddIntTest
+    {
+        [Test]
+        public void Tests()
+        {
+            Assert.AreEqual(5, OddInt.find_it(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 }));
+        }
+    }
+}
